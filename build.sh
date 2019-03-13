9a9fa5484354db4b98d67b517b4fdbea3008763f
Robert Kaussow <robert.kaussow@mail.schwarz>
2019-03-13 16:30:09 +0100

    switch CXX

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 yum install epel-release -y
 yum groupinstall 'Development Tools' -y
-yum install wget git which make cmake gcc gcc-c++ automake autoconf binutils-arm-linux-gnu glibc-devel.i686 libstdc++.i686 gcc-c++-arm-linux-gnu libgcc.i686 -y
+yum install wget git which -y
 
 mkdir -p $HOME/bin/arm-none-eabi-gcc/
 wget https://github.com/gnu-mcu-eclipse/arm-none-eabi-gcc/releases/download/v8.2.1-1.4/gnu-mcu-eclipse-arm-none-eabi-gcc-8.2.1-1.4-20190214-0604-centos32.tgz -q
@@ -21,7 +21,7 @@ tar xJf node-v$NODE_VERSION.tar.xz
 cd node-v$NODE_VERSION
 
 # build
-CC=arm-none-eabi-gcc CXX=arm-linux-gnu-g++ CC_host="gcc -m32" CXX_host="g++ -m32" ./configure --prefix=../install --dest-cpu=arm --cross-compiling --dest-os=linux --with-arm-float-abi=hard --with-arm-fpu=neon
+CC=arm-none-eabi-gcc CXX=arm-none-eabi-g++ CC_host="gcc -m32" CXX_host="g++ -m32" ./configure --prefix=../install --dest-cpu=arm --cross-compiling --dest-os=linux --with-arm-float-abi=hard --with-arm-fpu=neon
 make -j4
 make install
 
