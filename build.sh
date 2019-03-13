e158a0e83d805895eebb212a302a4baee7cfa34e
Robert Kaussow <robert.kaussow@mail.schwarz>
2019-03-13 16:10:55 +0100

    compile with arm-none-eabi-gcc

--- build.sh
+++ build.sh
@@ -10,19 +10,19 @@ tar -C $HOME/bin/arm-none-eabi-gcc/ -xzf gnu-mcu-eclipse-arm-none-eabi-gcc-8.2.1
 chmod -R -w $HOME/bin/arm-none-eabi-gcc/
 export PATH=$HOME/bin/arm-none-eabi-gcc/bin:$PATH
 
-ls -l $HOME/bin/arm-none-eabi-gcc/
-which arm-none-eabi-gcc
+# ls -l $HOME/bin/arm-none-eabi-gcc/
+# which arm-none-eabi-gcc
 
-# mkdir install
+mkdir install
 
-# # download and extract version tarball
-# wget -q https://nodejs.org/dist/v$NODE_VERSION/node-v$NODE_VERSION.tar.xz
-# tar xJf node-v$NODE_VERSION.tar.xz
-# cd node-v$NODE_VERSION
+# download and extract version tarball
+wget -q https://nodejs.org/dist/v$NODE_VERSION/node-v$NODE_VERSION.tar.xz
+tar xJf node-v$NODE_VERSION.tar.xz
+cd node-v$NODE_VERSION
 
-# # build
-# CC=arm-linux-gnu-gcc CXX=arm-linux-gnu-g++ CC_host="gcc -m32" CXX_host="g++ -m32" ./configure --prefix=../install --dest-cpu=arm --cross-compiling --dest-os=linux --with-arm-float-abi=hard --with-arm-fpu=neon
-# make -j4
-# make install
+# build
+CC=arm-none-eabi-gcc CXX=arm-linux-gnu-g++ CC_host="gcc -m32" CXX_host="g++ -m32" ./configure --prefix=../install --dest-cpu=arm --cross-compiling --dest-os=linux --with-arm-float-abi=hard --with-arm-fpu=neon
+make -j4
+make install
 
-# ls -l ../install
+ls -l ../install
