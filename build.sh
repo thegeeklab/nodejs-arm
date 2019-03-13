193f54a9daf36682c081cd867306f00547c213b7
Robert Kaussow <robert.kaussow@mail.schwarz>
2019-03-13 15:56:00 +0100

    switch toolchain

--- build.sh
+++ build.sh
@@ -2,18 +2,27 @@
 
 yum install epel-release -y
 yum groupinstall 'Development Tools' -y
-yum install wget git which make cmake gcc gcc-c++ automake autoconf binutils-arm-linux-gnu glibc-devel.i686 libstdc++.i686 gcc-arm-linux-gnu gcc-c++-arm-linux-gnu libgcc.i686 -y
+yum install wget git which make cmake gcc gcc-c++ automake autoconf binutils-arm-linux-gnu glibc-devel.i686 libstdc++.i686 gcc-c++-arm-linux-gnu libgcc.i686 -y
 
-mkdir install
+mkdir $HOME/bin/arm-none-eabi-gcc/
+wget https://github.com/gnu-mcu-eclipse/arm-none-eabi-gcc/releases/download/v8.2.1-1.4/gnu-mcu-eclipse-arm-none-eabi-gcc-8.2.1-1.4-20190214-0604-centos32.tgz
+tar -C $HOME/bin/arm-none-eabi-gcc/ -xzf gnu-mcu-eclipse-arm-none-eabi-gcc-8.2.1-1.4-20190214-0604-centos32.tgz --strip-components 3
+chmod -R -w $HOME/bin/arm-none-eabi-gcc/
+export PATH=$HOME/bin/arm-none-eabi-gcc:$PATH
 
-# download and extract version tarball
-wget -q https://nodejs.org/dist/v$NODE_VERSION/node-v$NODE_VERSION.tar.xz
-tar xJf node-v$NODE_VERSION.tar.xz
-cd node-v$NODE_VERSION
+ls -l $HOME/bin/arm-none-eabi-gcc/
+which arm-none-eabi-gcc
 
-# build
-CC=arm-linux-gnu-gcc CXX=arm-linux-gnu-g++ CC_host="gcc -m32" CXX_host="g++ -m32" ./configure --prefix=../install --dest-cpu=arm --cross-compiling --dest-os=linux --with-arm-float-abi=hard --with-arm-fpu=neon
-make -j4
-make install
+# mkdir install
 
-ls -l ../install
+# # download and extract version tarball
+# wget -q https://nodejs.org/dist/v$NODE_VERSION/node-v$NODE_VERSION.tar.xz
+# tar xJf node-v$NODE_VERSION.tar.xz
+# cd node-v$NODE_VERSION
+
+# # build
+# CC=arm-linux-gnu-gcc CXX=arm-linux-gnu-g++ CC_host="gcc -m32" CXX_host="g++ -m32" ./configure --prefix=../install --dest-cpu=arm --cross-compiling --dest-os=linux --with-arm-float-abi=hard --with-arm-fpu=neon
+# make -j4
+# make install
+
+# ls -l ../install
