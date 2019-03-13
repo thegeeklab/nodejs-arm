a5ea12013e5766d78f89a780162bb0c55241357b
Robert Kaussow <robert.kaussow@mail.schwarz>
2019-03-13 14:30:20 +0100

    test

--- build.sh
+++ build.sh
@@ -4,16 +4,17 @@ yum install epel-release -y
 yum groupinstall 'Development Tools' -y
 yum install wget git which make cmake gcc gcc-c++ automake autoconf binutils-arm-linux-gnu glibc-devel.i686 libstdc++.i686 gcc-arm-linux-gnu gcc-c++-arm-linux-gnu libgcc.i686 -y
 
-mkdir install
+which gcc-arm-linux-gnu
+# mkdir install
 
-# download and extract version tarball
-wget -q https://nodejs.org/dist/v$NODE_VERSION/node-v$NODE_VERSION.tar.xz
-tar xJf node-v$NODE_VERSION.tar.xz
-cd node-v$NODE_VERSION
+# # download and extract version tarball
+# wget -q https://nodejs.org/dist/v$NODE_VERSION/node-v$NODE_VERSION.tar.xz
+# tar xJf node-v$NODE_VERSION.tar.xz
+# cd node-v$NODE_VERSION
 
-# build
-CC=gcc-arm-linux-gnu CXX=gcc-c++-arm-linux-gnu CC_host="gcc -m32" CXX_host="g++ -m32" ./configure --prefix=../install --dest-cpu=arm --cross-compiling --dest-os=linux --with-arm-float-abi=hard --with-arm-fpu=neon
-make
-make install
+# # build
+# CC=gcc-arm-linux-gnu CXX=gcc-c++-arm-linux-gnu CC_host="gcc -m32" CXX_host="g++ -m32" ./configure --prefix=../install --dest-cpu=arm --cross-compiling --dest-os=linux --with-arm-float-abi=hard --with-arm-fpu=neon
+# make
+# make install
 
-ls -l ../install
+# ls -l ../install
