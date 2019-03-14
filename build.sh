dc0b0e34b2dfa3a8d8cdcf2b69d42b5eab490717
Robert Kaussow <robert.kaussow@mail.schwarz>
2019-03-14 15:52:33 +0100

    test

--- build.sh
+++ build.sh
@@ -10,28 +10,30 @@ yum groupinstall 'Development Tools' -y
 yum install wget git which make cmake automake autoconf glibc-devel.i686 libstdc++.i686 libgcc.i686 -y
 # yum install wget git which -y
 
-cd $DRONE_HOME/compiler/
-git clone https://github.com/rvagg/rpi-newer-crosstools .
-export PATH=$DRONE_HOME/compiler/x64-gcc-4.9.4-binutils-2.28/arm-rpi-linux-gnueabihf/bin:$PATH
+strings /lib/libstdc++.so.6 | grep GLIBCXX
 
-# ls -l $HOME/bin/arm-none-eabi-gcc/
-# which arm-rpi-linux-gnueabihf-gcc
-# which arm-rpi-linux-gnueabihf-g++
+# cd $DRONE_HOME/compiler/
+# git clone https://github.com/rvagg/rpi-newer-crosstools .
+# export PATH=$DRONE_HOME/compiler/x64-gcc-4.9.4-binutils-2.28/arm-rpi-linux-gnueabihf/bin:$PATH
 
-# arm-rpi-linux-gnueabihf-gcc --verbose
+# # ls -l $HOME/bin/arm-none-eabi-gcc/
+# # which arm-rpi-linux-gnueabihf-gcc
+# # which arm-rpi-linux-gnueabihf-g++
 
-cd $DRONE_HOME
-# download and extract version tarball
-wget -q https://nodejs.org/dist/v$NODE_VERSION/node-v$NODE_VERSION.tar.xz
-tar xJf node-v$NODE_VERSION.tar.xz
-cd $DRONE_HOME/node-v$NODE_VERSION
+# # arm-rpi-linux-gnueabihf-gcc --verbose
 
-# build
-CC="arm-rpi-linux-gnueabihf-gcc -march=armv7-a" CXX="arm-rpi-linux-gnueabihf-g++ -march=armv7-a" CC_host="gcc -m32" CXX_host="g++ -m32" ./configure --prefix=$DRONE_HOME/install --dest-cpu=arm --cross-compiling --dest-os=linux --with-arm-float-abi=hard --with-arm-fpu=neon
-make -j 8
-make install
+# cd $DRONE_HOME
+# # download and extract version tarball
+# wget -q https://nodejs.org/dist/v$NODE_VERSION/node-v$NODE_VERSION.tar.xz
+# tar xJf node-v$NODE_VERSION.tar.xz
+# cd $DRONE_HOME/node-v$NODE_VERSION
 
-cd $DRONE_HOME/install
-tar -zcf ../dist/node-v$NODE_VERSION-linux-armv7.tar.gz .
+# # build
+# CC="arm-rpi-linux-gnueabihf-gcc -march=armv7-a" CXX="arm-rpi-linux-gnueabihf-g++ -march=armv7-a" CC_host="gcc -m32" CXX_host="g++ -m32" ./configure --prefix=$DRONE_HOME/install --dest-cpu=arm --cross-compiling --dest-os=linux --with-arm-float-abi=hard --with-arm-fpu=neon
+# make -j 8
+# make install
 
-ls -l $DRONE_HOME/dist
+# cd $DRONE_HOME/install
+# tar -zcf ../dist/node-v$NODE_VERSION-linux-armv7.tar.gz .
+
+# ls -l $DRONE_HOME/dist
