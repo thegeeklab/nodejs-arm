02bbb8722b434c571a18c20ed1312f26a19d2a14
Robert Kaussow <robert.kaussow@mail.schwarz>
2019-03-14 11:59:05 +0100

    compile with -march=armv7-a

--- build.sh
+++ build.sh
@@ -25,8 +25,8 @@ tar xJf node-v$NODE_VERSION.tar.xz
 cd node-v$NODE_VERSION
 
 # build
-CC="arm-rpi-linux-gnueabihf-gcc -march=armv7-a" CXX="arm-rpi-linux-gnueabihf-g++ -march=armv7-a" CCFLAGS="-march=armv7-a" CXXFLAGS="-march=armv7-a"  CC_host="gcc -m32" CXX_host="g++ -m32" ./configure --prefix=../install --dest-cpu=arm --cross-compiling --dest-os=linux --with-arm-float-abi=hard --with-arm-fpu=neon
-# make --j
+CC="arm-rpi-linux-gnueabihf-gcc -march=armv7-a" CXX="arm-rpi-linux-gnueabihf-g++ -march=armv7-a" CC_host="gcc -m32" CXX_host="g++ -m32" ./configure --prefix=../install --dest-cpu=arm --cross-compiling --dest-os=linux --with-arm-float-abi=hard --with-arm-fpu=neon
+make --j
 # make install
 
 ls -l ../install
