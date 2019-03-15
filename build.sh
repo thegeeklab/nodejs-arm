a70863749a9ec1a104ac2fc158db21fd6af69d89
Robert Kaussow <robert.kaussow@mail.schwarz>
2019-03-15 09:02:01 +0100

    compile with static libstdc++

--- build.sh
+++ build.sh
@@ -10,18 +10,10 @@ yum groupinstall 'Development Tools' -y
 yum install wget git which make cmake automake autoconf glibc-devel.i686 libstdc++.i686 libgcc.i686 -y
 
 cd $DRONE_HOME/compiler/
-# git clone https://github.com/rvagg/rpi-newer-crosstools .
-# export PATH=$DRONE_HOME/compiler/x64-gcc-4.9.4-binutils-2.28/arm-rpi-linux-gnueabihf/bin:$PATH
-git clone https://github.com/raspberrypi/tools .
-export PATH=$DRONE_HOME/compiler/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin:$PATH
-
-
-
-# ls -l $HOME/bin/arm-none-eabi-gcc/
-# which arm-rpi-linux-gnueabihf-gcc
-# which arm-rpi-linux-gnueabihf-g++
-
-# arm-rpi-linux-gnueabihf-gcc --verbose
+git clone https://github.com/rvagg/rpi-newer-crosstools .
+export PATH=$DRONE_HOME/compiler/x64-gcc-4.9.4-binutils-2.28/arm-rpi-linux-gnueabihf/bin:$PATH
+# git clone https://github.com/raspberrypi/tools .
+# export PATH=$DRONE_HOME/compiler/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin:$PATH
 
 cd $DRONE_HOME
 # download and extract version tarball
@@ -30,8 +22,8 @@ tar xJf node-v$NODE_VERSION.tar.xz
 cd $DRONE_HOME/node-v$NODE_VERSION
 
 # build
-# CC="arm-rpi-linux-gnueabihf-gcc -march=armv7-a" CXX="arm-rpi-linux-gnueabihf-g++ -march=armv7-a" CC_host="gcc -m32" CXX_host="g++ -m32" ./configure --prefix=$DRONE_HOME/install --dest-cpu=arm --cross-compiling --dest-os=linux --with-arm-float-abi=hard --with-arm-fpu=neon
-CC="arm-linux-gnueabihf-gcc -march=armv7-a" CXX="arm-linux-gnueabihf-g++ -march=armv7-a" CC_host="gcc -m32" CXX_host="g++ -m32" ./configure --prefix=node-v$NODE_VERSION --dest-cpu=arm --cross-compiling --dest-os=linux --with-arm-float-abi=hard --with-arm-fpu=neon
+CC="arm-rpi-linux-gnueabihf-gcc -march=armv7-a -static-libstdc++" CXX="arm-rpi-linux-gnueabihf-g++ -march=armv7-a -static-libstdc++" CC_host="gcc -m32" CXX_host="g++ -m32" ./configure --prefix=$DRONE_HOME/install --dest-cpu=arm --cross-compiling --dest-os=linux --with-arm-float-abi=hard --with-arm-fpu=neon
+# CC="arm-linux-gnueabihf-gcc -march=armv7-a" CXX="arm-linux-gnueabihf-g++ -march=armv7-a" CC_host="gcc -m32" CXX_host="g++ -m32" ./configure --prefix=node-v$NODE_VERSION --dest-cpu=arm --cross-compiling --dest-os=linux --with-arm-float-abi=hard --with-arm-fpu=neon
 
 make -j 8
 make install DESTDIR=$DRONE_HOME/install/ PORTABLE=1
